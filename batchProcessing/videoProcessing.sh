7743f06bc21ea98a3e183f83434f970e5b9df5ec
Santiago Hernandez <s.hernandezf@uniandes.edu.co>
2024-05-01 01:06:59 -0500

    aws adapt

--- batchProcessing/videoProcessing.sh
+++ batchProcessing/videoProcessing.sh
@@ -1,16 +1,24 @@
-#!/bin/bash
+#!/usr/bin/bash
 
 # Check if an argument is provided
 if [ $# -ne 1 ]; then
-    echo "Usage: $0 <video_path>"
+    echo "Usage: $0 <video_name>"
     exit 1
 fi
 
-input_video="$1"
+video_name="$1"
+input_video="/home/ubuntu/${video_name}"
+processed_folder="/home/ubuntu"
+output_video="${processed_folder}/${video_name%.*}_processed.mp4"
 
-# Final filename
-processed_folder="/mnt/nfs/general/processed/"
-output_video="${processed_folder}/$(basename ${input_video%.*}_processed.mp4)"
+# Fetch the video from S3
+aws s3 cp "s3://nubeandesgrupocool/unprocessed/${video_name}" "$input_video"
+
+# Check if download was successful
+if [ ! -f "$input_video" ]; then
+    echo "Failed to download video."
+    exit 1
+fi
 
 # Create processed directory if it does not exist
 if [ ! -d "$processed_folder" ]; then
@@ -20,6 +28,7 @@ fi
 initial_frame="./idrl.jpg"
 final_frame="./idrl.jpg"
 
+# Get video dimensions
 video_width=$(ffprobe -v error -select_streams v:0 -show_entries stream=width -of csv=s=x:p=0 "$input_video")
 video_height=$(ffprobe -v error -select_streams v:0 -show_entries stream=height -of csv=s=x:p=0 "$input_video")
 
@@ -33,3 +42,12 @@ ffmpeg -i "$input_video" -i "$initial_frame" -i "$final_frame" \
 -c:v libx264 -c:a copy -t 20 "$output_video"
 
 echo "The processed video has been saved as: $output_video"
+
+# Upload processed video to S3
+aws s3 cp "$output_video" "s3://nubeandesgrupocool/processed/"
+
+# Delete local copies of the video
+rm "$input_video"
+rm "$output_video"
+
+echo "Local copies of videos have been deleted."
